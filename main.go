0b03308962ba114ba57caea2b96efbea292a1cf2
kaitsuabaka <kaitsuabaka@gmail.com>
2021-08-26 16:58:23 -0500

    code -> hello world sprite and deleted some assets

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"image"
+	_ "image/png"
+	"os"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
+
 func run() {
+
 	cfg := pixelgl.WindowConfig{
 		Title:  "Ciel!",
 		Bounds: pixel.R(0, 0, 1024, 768),
@@ -16,8 +34,15 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+	pic, err := loadPicture("assets/simple_space/PNG/Retina/ship_A.png")
+	if err != nil {
+		panic(err)
+	}
 
-	win.Clear(colornames.Skyblue)
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
+	win.Clear(colornames.Black)
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 
 	for !win.Closed() {
 		win.Update()
